ed3e5c3d8474dc7d4869d537f93cd429f2605042
Radu Alexe <raduandrei.alexe@freescale.com>
2015-08-04 17:54:05 +0300

    added script to configure git

---
+++ config.sh
@@ -0,0 +1,11 @@
+# Author: Alexe Radu-Andrei
+
+# GIT configuration
+GIT_USER="Radu Alexe"
+GIT_EMAIL="raduandrei.alexe@freescale.freescale.com"
+GIT_EDITOR="vim"
+
+git config --global user.name $GIT_USER
+git config --global user.email $GIT_EMAIL
+git config --global color.ui always
+git config --global core.editor $GIT_EDITOR
