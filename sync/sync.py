2a7e417ed03ea8e7c38e65fd5d9ac9011e787223
Radu Alexe <raduandrei.alexe@freescale.com>
2015-08-04 19:58:40 +0300

    simplification
    for now the setup is going to only hold three files:
    - .bashrc
    - .vimrc
    - .gitconfig
    
    and a script "config.sh" that can update/save the current configuration
    - on update the configurations are copied to the ~/home directory
    - on save the configuration files are saved from the ~/home directory

--- sync/sync.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-
-import log
-
-# local config files are relative to the root directory
-relations = [['./config/bashrc', '~/.bashrc'],
-	         ['./config/vimrc',  '~/.vimrc']];
-
-log.info("Synching started")
