1998d1ebc263fb2fb0d4d559a755618ec8f9f9d7
Radu Alexe <alexeradu2007@gmail.com>
2017-11-19 00:13:12 +0200

    git: update .gitconfig based on script
    The old method was to copy a saved version of .gitconfig to
    ~ directory. The problem comes when dealing with multiple settings
    (ex. work and home) that require different settings. In such
    circumstances a programing/scripting language must be used to
    detect the existing environment and to match the git configuration
    accordingly.
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

---
+++ config-git.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+is_work_machine() {
+	local users=(b47441 B47441)
+	local login=$(whoami)
+
+	for user in $users; do
+		if [ $user = $login ]; then
+			echo 1
+			return
+		fi
+	done
+}
+
+is_personal_machine() {
+	local users=(radu)
+	local login=$(whoami)
+
+	for user in $users; do
+		if [ $user = $login ]; then
+			echo 1
+			return
+		fi
+	done
+}
+
+USERNAME="Radu Alexe"
+EDITOR="vim"
+
+if [ -n  "$(is_work_machine)" ]; then
+	echo "Really"
+	EMAIL="radu.alexe@nxp.com"
+elif [ -n "$(is_personal_machine)" ]; then
+	EMAIL="alexeradu2007@gmail.com"
+else
+	echo "User $(whoami) not in the predefined database. Insert new credentials"
+
+	echo "Username: "; read USERNAME
+	echo "Email: "; read EMAIL
+	echo "Editor: "; read EDITOR
+fi
+
+git config --global user.name "$USERNAME"
+git config --global user.email "$EMAIL"
+git config --global core.editor "$EDITOR"
