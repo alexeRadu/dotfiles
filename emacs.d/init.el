256473e691cb072a2d7c3b15e7fc3e8a01ec0a3f
Radu Alexe <alexeradu2007@gmail.com>
2018-06-03 23:53:05 +0300

    add emacs config files to repo
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

---
+++ emacs.d/init.el
@@ -0,0 +1,50 @@
+
+;; Packages
+(package-initialize)
+
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;; Interface
+;; Don't show the menu-bar
+(menu-bar-mode -1)
+
+;; Don't show the scroll-bar
+(toggle-scroll-bar -1)
+
+;; Don't show the tool-bar
+(tool-bar-mode -1)
+
+;; Theming
+(load-theme 'wombat)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
