194a5fd00630080315512d74c33934e5416b5d1f
Radu Alexe <alexeRadu2007@gmail.com>
2018-06-13 10:56:29 +0300

    emacs: use ace-window for window switching
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -42,12 +42,21 @@
 
 (defalias 'list-buffers 'ibuffer)
 
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (ace-window use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
