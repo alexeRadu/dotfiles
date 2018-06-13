7e1cef759132fe89b32541407fe0673fd9e4b1f0
Radu Alexe <alexeRadu2007@gmail.com>
2018-06-13 12:18:10 +0300

    emacs: add auto-complete package
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -83,12 +83,22 @@
   :ensure t
   :bind (("M-s" . avy-goto-char)))
 
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (counsel swiper ace-window use-package))))
+ '(package-selected-packages
+   (quote
+    (auto-complete counsel swiper ace-window use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
