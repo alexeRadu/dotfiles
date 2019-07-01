b7f249625cf2ef58c082e06e816e9b8d1c6d31b0
Radu Alexe <alexeRadu2007@gmail.com>
2019-07-01 15:08:14 +0300

    emacs: save last changes
    I don't know exactly why they were made. But for the moment I don't use
    emacs therefore I'll update it's config when the time comes.
    Just saving the current modifications so they might not go to waste.
    
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,7 +14,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rtags treemacs-evil smacemacs-theme zenburn-theme which-key use-package try spacemacs-theme org-bullets counsel color-theme auto-complete ace-window))))
+    (rtags treemacs-evil smacemacs-theme zenburn-theme which-key use-package try spacemacs-theme org-bullets counsel color-theme auto-complete ace-window)))
+ '(realgud:gdb-command-name "~/riscv32-unknown-elf-gcc/bin/riscv32-unknown-elf-gdb"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
