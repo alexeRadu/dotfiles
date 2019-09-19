e2160f040f06d6a8ea35a158ac5671a15af8cda1
Radu Alexe <alexeRadu2007@gmail.com>
2019-09-19 11:09:10 +0300

    vim: dbug: add logging
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- vim/bin/python3/dbug/main.py
+++ vim/bin/python3/dbug/main.py
@@ -1,4 +1,5 @@
 from select import select
+import logging
 import json
 import sys
 
@@ -9,7 +10,15 @@ def vim_echo(msg):
     print(json.dumps(cmd), file=sys.stdout)
     sys.stdout.flush()
 
+handler = logging.FileHandler('/tmp/dbug.log', 'w')
+handler.formatter = logging.Formatter('%(msecs)6d %(levelname)-5s   %(message)s')
+logging.root.addHandler(handler)
+logging.root.setLevel(logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
 vim_echo("GDB server started")
+logger.info("GDB server started")
 
 while True:
     ready, _, _ = select([sys.stdin], [], [], 2)
@@ -18,6 +27,8 @@ while True:
         continue
 
     index, msg = json.loads(sys.stdin.readline())
+
     msg = str(msg)
+    logger.debug("Rcvd: %d - %s" % (index, msg))
 
     vim_echo(msg)
