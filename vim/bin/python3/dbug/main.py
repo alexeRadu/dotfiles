7031df905a152c952bc69cc3bae4acc1dbe4cadd
Radu Alexe <alexeRadu2007@gmail.com>
2019-10-15 18:26:15 +0300

    vim: dbug: implement class Breakpoint()
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- vim/bin/python3/dbug/main.py
+++ vim/bin/python3/dbug/main.py
@@ -17,47 +17,76 @@ vim.echo("GDB server started")
 logger.info("\n\n\n\n\n")
 logger.info("GDB server started")
 
+
+class Breakpoint(object):
+    def __init__(self, filepath, line):
+        self.filepath = filepath
+        self.line     = line
+        self.number   = None
+
+    def location(self):
+        return "%s:%d" % (self.filepath, self.line)
+
+    def insert(self):
+        global bkpt_info
+
+        response = gdbmi.write("-break-insert %s" % (bp.location()))
+        parse_response(response)
+
+        if bkpt_info:
+            self.number = bkpt_info["number"]
+
+    def inserted(self):
+        return True if self.number != None else False
+
+    def remove(self):
+        response = gdbmi.write("-break-delete %s" % (self.number))
+        parse_response(response)
+
+    def place(self):
+        expr = "sign place %s line=%d name=dbg_bp file=%s" % (self.number, self.line, self.filepath)
+        vim.execute(expr)
+        vim.redraw()
+
+    def unplace(self):
+        expr = "sign unplace %s" % (self.number)
+        vim.execute(expr)
+        vim.redraw()
+
+
 class BreakpointDB(object):
     def __init__(self):
+        # TODO: investigate better solution for storing breakpoints
         self.breakpoints = []
 
-    def __contains__(self, item):
-        for bp in self.breakpoints:
-            if bp["filename"] == item["filename"] and bp["line"] == item["line"]:
-                return True
+    def get(self, filepath, line):
+        bkpt = Breakpoint(filepath, line)
 
-        return False
-
-    def get(self, item):
         for bp in self.breakpoints:
-            if bp["filename"] == item["filename"] and bp["line"] == item["line"]:
-                self.breakpoints.remove(bp)
+            if bp.location() == bkpt.location():
                 return bp
 
-        return None
+        self.breakpoints.append(bkpt)
+        return bkpt
 
-    def add(self, bp):
-        self.breakpoints.append(bp)
-
+    def remove(self, bp):
+        self.breakpoints.remove(bp)
 
 bpdb = BreakpointDB()
 
-
-def get_result(response):
-    for msg in response:
-        if msg["type"] == "result":
-            return msg
-
 gdb_is_running = False
 gdb_is_debugging = False
+bkpt_info = None
 result = None
 pc = None
 
 def parse_response(response):
     global result
+    global bkpt_info
 
     unused = []
     result = None
+    bkpt_info = None
 
     for r in response:
         if r["type"] == "notify":
@@ -91,6 +120,10 @@ def parse_response(response):
         elif r["type"] == "result":
             result = r["message"]
 
+            if r["payload"] and "bkpt" in r["payload"]:
+                bkpt_info = r["payload"]["bkpt"]
+                unused.append(r)
+
         elif r["type"] == "console":
             # ignore cosole output for now
             pass
@@ -119,36 +152,27 @@ try:
             logger.debug("GDB loaded exec and symbols file: %s" % filepath)
 
         elif msg["name"] == "toggle-breakpoint":
-            if msg not in bpdb:
-                location = msg["filename"] + ":" +  str(msg["line"])
-                response = gdbmi.write("-break-insert " + location)
-                logger.debug("gdb response: \n" + pprint.pformat(response))
+            filepath = msg["filename"]
+            line = msg["line"]
 
-                result = get_result(response)
-                if result["message"] != "done":
+            bp = bpdb.get(filepath, line)
+
+            if not bp.inserted():
+                bp.insert()
+
+                if not result or result == "error":
                     continue
 
-                bp = {"filename": msg["filename"], "line": msg["line"]}
-                bp["number"] = int(result["payload"]["bkpt"]["number"])
-                bpdb.add(bp)
-
-                expr = "sign place %d line=%d name=dbg_bp file=%s" % (bp["number"], bp["line"], bp["filename"])
-                vim.execute(expr)
-                vim.redraw()
-
+                bp.place()
             else:
-                bp = bpdb.get(msg)
+                bp.remove()
 
-                response = gdbmi.write("-break-delete %d" % (bp["number"]))
-                logger.debug("gdb response: \n" + pprint.pformat(response))
-
-                result = get_result(response)
-                if result["message"] != "done":
+                if not result or result == "error":
                     continue
 
-                expr = "sign unplace %d" % (bp["number"])
-                vim.execute(expr)
-                vim.redraw()
+                bp.unplace()
+                bpdb.remove(bp)
+
         elif msg["name"] == "continue":
 
             if not gdb_is_debugging:
