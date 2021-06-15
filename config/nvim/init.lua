66751163e55cafe405b9764be3117242ca1f98fd
Radu Alexe <alexeradu2007@gmail.com>
2021-06-15 20:09:12 +0300

    nvim: add mappings for dbug
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,21 @@
 local api = vim.api
 
+local function map(mode, lhs, rhs, opts)
+	local options = {noremap = true}
+
+	if opts then
+		options = vim.tbl_extend('force', options, opts)
+	end
+
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Mappings for dbug plugin
+map('', '<f5>',  ':Dbg<CR>')
+map('', '<f9>',  ':DbgBreakpoint<CR>')
+map('', '<f10>', ':DbgNext<CR>')
+map('', '<f11>', ':DbgStep<CR>')
+
 api.nvim_command('set runtimepath ^=~/.vim')
 api.nvim_command('set runtimepath +=~/.vim/after')
 api.nvim_command('source ~/.vimrc')
