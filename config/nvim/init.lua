e318bf96f964ab848c6555109dd80f6bcef7dff4
Radu Alexe <alexeradu2007@gmail.com>
2022-03-23 13:55:20 +0200

    nvim: carry some configs from vim
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,6 +7,12 @@ api.nvim_set_keymap("n", "<F5>", ":luafile %<CR>", {})
 
 vim.g.mapleader = " "
 
+-- settings
+vim.o.number = true
+vim.o.rnu = true
+--vim.o.cursorline = true
+vim.o.mouse = 'a'
+
 require("plugins")
 
 api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<cr>", {})
