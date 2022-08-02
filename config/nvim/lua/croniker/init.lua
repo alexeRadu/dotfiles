b48bb4191936bdef2eb62705b72434c9b406befe
Radu Alexe <alexeradu2007@gmail.com>
2022-08-02 19:01:44 +0300

    croniker.nvim: add toggling functionlity
    This way, with the same command, one can close the window.
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- config/nvim/lua/croniker/init.lua
+++ config/nvim/lua/croniker/init.lua
@@ -1,8 +1,8 @@
 local popup = require("plenary.popup")
 
 local M = {}
-local buf = nil
-local win = nil
+local buf   = nil
+local win   = nil
 local timer = vim.loop.new_timer()
 local opts = {
     title = "Croniker",
@@ -11,24 +11,20 @@ local opts = {
 }
 
 
-function M.close_window()
+function close_window()
     if win then
         vim.api.nvim_win_close(win, true)
+        win = nil
+        buf = nil
     end
 
-    timer:stop()
+    if timer then
+        timer:stop()
+        timer = nil
+    end
 end
 
-function M.create_window()
-    -- Always create a new window
-    -- this will lead to a performance penalty that is acceptable but the
-    -- code will be simples
- 
-    if win then
-        win = nil
-    end
-
-    -- Get the time info to be displayed
+function create_window()
     local info = {
         os.date("%A %d/%m/%Y", os.time()),
         "ww" .. os.date("%W", os.time()),
@@ -38,7 +34,9 @@ function M.create_window()
     win = popup.create(info, opts)
     buf = vim.api.nvim_win_get_buf(win)
 
-    vim.api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('croniker').close_window()<cr>", {silent = true})
+    vim.api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('croniker').toggle_window()<cr>", {silent = true})
+
+    timer = vim.loop.new_timer()
 
     timer:start(1000, 1, vim.schedule_wrap(function()
         local time     = os.date("%H:%M:%S", os.time())
@@ -47,6 +45,14 @@ function M.create_window()
 
         vim.api.nvim_buf_set_text(buf, pad_top + 2, pad_left, pad_top + 2, pad_left + #time, {time})
     end))
+end
+
+function M.toggle_window()
+    if win then
+        close_window()
+    else
+        create_window()
+    end
 end
 
 return M
