811fb8f37324a9528cd02fd53f24500e94aba2fe
Radu Alexe <alexeradu2007@gmail.com>
2022-08-30 21:11:38 +0300

    nvim: set tabsize to different values depending on the file location
    For telescope lua files the author has decided to use 2 spaces for each
    tab. So I'm detecting that the file is located inside the neovim data
    directory and if so I'm setting the tabsize to 2 spaces.
    
    Further on maybe additional filtering may be necessary since some plugin
    authors may have slightly different preferences but for now it will
    suffice.
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 * Comment.nvim: - implement comment operator mapping (delete/select/change ... comment)
-* lua: set tabstop + shiftwidth=2 for some directories (like telescope + other?)
 * module-reload: function to reload current files
 * module-reload: automatic reload (for modules inside config - .local - files)
 * module-reload: telescope plugin (show existing loaded modules + keybinding to unload/reload/goto different modules)
--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -1,8 +1,25 @@
-local o = vim.o
+local o   = vim.o
+local api = vim.api
 
-o.tabstop     = 4
-o.shiftwidth  = 4
-o.softtabstop = 4
-o.expandtab   = true
+
+local path = api.nvim_buf_get_name(0)
+local stdpath = api.nvim_eval('stdpath("data")')
+
+
+if string.match(path, stdpath) then
+    -- this means we are accesing one of the lua files that is inside the
+    -- neovim DATA path; usually here the plugin authors have opted to use
+    -- 2 spaces for a tab rather then 4
+    o.tabstop     = 2
+    o.shiftwidth  = 2
+    o.softtabstop = 2
+else
+    o.tabstop     = 4
+    o.shiftwidth  = 4
+    o.softtabstop = 4
+end
+
+o.expandtab = true
+
 
 bind_key('n', '<leader><cr>', ':luafile %<cr>')
