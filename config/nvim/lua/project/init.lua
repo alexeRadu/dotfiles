5e45425b46d6a9f88072ba6c0f9a4148796bdcef
Radu Alexe <alexeradu2007@gmail.com>
2022-09-30 13:50:17 +0300

    nvim.project: add additional functionality:
    - goto project base directory
    - set <leader>pf as a keybinding to find_files using telescope through
      the list of files specified in the config
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- config/nvim/lua/project/init.lua
+++ config/nvim/lua/project/init.lua
@@ -11,7 +11,7 @@ local projects = {
         search_dirs = {
             "../sdk-rw610/middleware/wireless/framework",
             "../sdk-rw610/middleware/wireless/ieee-802.15.4",
-            ".",
+            "./src/rw/rw610",
         },
     },
     {
@@ -23,14 +23,36 @@ local projects = {
             ".",
         },
     },
+    {
+        name = "project.nvim",
+        path = "~/.config/nvim",
+        search_dirs = {
+            ".",
+        },
+    },
 }
 
 local M = {}
 
 M.config = {
     current_loaded_project = nil,
+    find_files_keys = '<leader>pf',
 }
 
+-- call :Telescope find_files on specific search_dirs
+-- M.config.current_loaded_project must be set to one of the projects
+-- specified in the config
+function M.project_find_files()
+    if M.config.current_loaded_project == nil then
+        return
+    end
+
+    local project = M.config.current_loaded_project
+
+    require("telescope.builtin.__files").find_files({
+        search_dirs = project.search_dirs,
+    })
+end
 
 local function open_project(prompt_bufnr)
     local selected_entry = state.get_selected_entry(prompt_bufnr)
@@ -41,11 +63,13 @@ local function open_project(prompt_bufnr)
 
     project_name = selected_entry.value
     if M.config.current_loaded_project and M.config.current_loaded_project["name"] == project_name then
-        print(string.format("Project %s already loaded", project_name))
         actions.close(prompt_bufnr)
+        print(string.format("Project %s already loaded", project_name))
         return
     end
 
+    -- Do we need this? The list of project names is taken from the same 'projects' array so
+    -- it's expected that the name will be found in the list of projects
     for _, project in ipairs(projects) do
         if project["name"] == project_name then
             M.config.current_loaded_project = project
@@ -53,10 +77,18 @@ local function open_project(prompt_bufnr)
         end
     end
 
-    -- TODO: change current directory using project[path]
-    -- TODO: use project[search_dir} for Telescope
-
+    project = M.config.current_loaded_project
     actions.close(prompt_bufnr)
+
+    if project.path ~= vim.fn.getcwd() then
+        -- TODO: remember the last working-directory? in case of exiting from any project
+        -- maybe have a history to go bach in history to different projects/working-directories
+        vim.api.nvim_set_current_dir(project.path)
+    end
+
+    -- TODO: do we need to remove the mapping before setting?
+    -- what about removing the keymap set when exiting any project?
+    vim.keymap.set('n', M.config.find_files_keys, M.project_find_files, {noremap = true, silent = true})
 end
 
 function M.projects()
