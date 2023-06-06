27f5b60ae10fd4122e2b201c10f35bd2b6888f1b
Radu Alexe <alexeradu2007@gmail.com>
2023-06-06 16:43:57 +0300

    nvim: replace sumneko command (deprecated)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -245,19 +245,19 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local sumneko_cmd = nil
+local lua_ls_cmd = nil
 if os.getenv("NAME") == "NXL49106" then
-    sumneko_cmd = { "/home/" .. os.getenv("USER") .. "/code/lua-language-server/bin/lua-language-server" }
+    lua_ls_cmd = { "/home/" .. os.getenv("USER") .. "/code/lua-language-server/bin/lua-language-server" }
 elseif vim.fn.system('hostname') == 'tp\n' then
-    sumneko_cmd = { "/home/" .. os.getenv("USER") .. "/lua-language-server/bin/lua-language-server" }
+    lua_ls_cmd = { "/home/" .. os.getenv("USER") .. "/lua-language-server/bin/lua-language-server" }
 elseif vim.fn.system('hostname')  == 'asr\n' then
-    sumneko_cmd = { "/home/" .. os.getenv("USER") .. "/lua-language-server/bin/lua-language-server" }
+    lua_ls_cmd = { "/home/" .. os.getenv("USER") .. "/lua-language-server/bin/lua-language-server" }
 end
 
 vim.api.nvim_set_hl(0, "LspComment", { link = "Comment" })
 
-require('lspconfig').sumneko_lua.setup({
-    cmd = sumneko_cmd,
+require('lspconfig').lua_ls.setup({
+    cmd = lua_ls_cmd,
     on_attach = on_attach,
     capabilities = capabilities,
     settings = {
