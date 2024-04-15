f28b748e909a51675a504f87446b724f0cff6673
Radu Alexe <alexeradu2007@gmail.com>
2024-04-15 18:41:33 +0300

    nvim: change keybindings for window navigation

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,13 +24,28 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>i', ':edit $MYVIMRC<CR>', { silent = true })
 vim.keymap.set('n', '<leader>e', ':luafile %<CR>', { silent = true })
 vim.keymap.set('n', '<leader>h', ':help ', { silent = false })
+vim.keymap.set('n', '<C-s>', ':w<CR>', { silent = false })
+
+-- Keybindings for navigating/creating windows
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<C-w>h', '<Nop>', { silent = true })
+vim.keymap.set('n', '<C-w>l', '<Nop>', { silent = true })
+vim.keymap.set('n', '<C-w>j', '<Nop>', { silent = true })
+vim.keymap.set('n', '<C-w>k', '<Nop>', { silent = true })
+vim.keymap.set('n', '<C-w>h', ':leftabove vsplit<CR>', { silent = true })
+vim.keymap.set('n', '<C-w>l', ':rightbelow vsplit<CR>', { silent = true })
+vim.keymap.set('n', '<C-w>j', ':rightbelow split<CR>', { silent = true })
+vim.keymap.set('n', '<C-w>k', ':leftabove split<CR>', { silent = true })
 
 -- Terminal keybindings
 vim.keymap.set('t', '<C-z>', [[<C-\><C-n>]], { silent = true })
-vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h', { silent = true})
-vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l', { silent = true})
-vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k', { silent = true})
-vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j', { silent = true})
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', { silent = true})
+vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', { silent = true})
+vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', { silent = true})
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', { silent = true})
 
 -- open help in a horizontal split
 local horizontal_help_split_group = vim.api.nvim_create_augroup("HorizontalHelpSplit", { clear = true })
