fd602a5e20513653476072bdfed9e65f2ea2d575
Alexe Radu <radu@Alexes-MacBook-Pro.local>
2024-04-28 22:47:10 +0300

    update install scripts

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-pkgs=(chromium			# currently my browser
-	make pkgconf gcc	# usefull for building stuff
-	python-pip		# for python packages
-	sddm			#
-	qt5-graphicaleffects	# additional for sddm
-	qt5-quickcontrols	#
-	openssh)		# provides: ssh-keygen
+pkgs=(chromium              # currently my browser
+	make pkgconf gcc        # usefull for building stuff
+	python-pip              # for python packages
+	sddm                    #
+	qt5-graphicaleffects    # additional for sddm
+	qt5-quickcontrols       #
+	openssh)                # provides: ssh-keygen
 
 echo "Installing packages"
 
 for pkg in ${pkgs[@]}; do
-	printf "%-16s : " $pkg
+    printf "%-16s : " $pkg
 
-	if [ -z "$(pacman -Qi $pkg 2>/dev/null)" ]; then
-		sudo pacman -Sqyu --noconfirm $pkg 2>&1 1>/dev/null
-		printf "done\n"
-	else
-		printf "already exists\n"
-	fi
+    if [ -z "$(pacman -Qi $pkg 2>/dev/null)" ]; then
+        sudo pacman -Sqyu --noconfirm $pkg 2>&1 1>/dev/null
+        printf "done\n"
+    else
+        printf "already exists\n"
+    fi
 done
