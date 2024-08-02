961e5be4ec64d38c5ac664b7b830ef8bffd7e13f
Radu Alexe <alexeradu2007@gmail.com>
2024-08-02 13:42:10 +0300

    nvim: fix repo reference

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -60,7 +60,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     is_bootstrap = true
-    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomas/packer.nvim', install_path }
+    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
     vim.cmd [[packadd packer.nvim]]
 end
 
