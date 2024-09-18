af18a069614b7aad20c7424dcb85a6fc4f1bc4f5
Radu Alexe <alexeRadu2007@gmail.com>
2024-09-18 12:57:51 +0300

    nvim: ufo: update keybindings

--- config/nvim/lua/packages/ufo.lua
+++ config/nvim/lua/packages/ufo.lua
@@ -12,7 +12,11 @@ M.post_setup = function()
     vim.o.foldlevel      = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable     = true
+    vim.o.fillchars      = [[eob: ,fold: ,foldopen:-,foldsep: ,foldclose:+]]
 
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zm', require('ufo').closeAllFolds)
+    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
     vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith)
 end
 
