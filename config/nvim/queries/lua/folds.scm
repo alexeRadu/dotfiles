93ead31b9c6eaa010f64621e938477d5ac351a48
Radu Alexe <alexeradu2007@gmail.com>
2025-04-24 15:56:14 +0300

    nvim: add custom folds.scm for lua and c
    They just add '(comment)+' to the default config just so that it folds
    adjacent lines of comments into one block.
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

---
+++ config/nvim/queries/lua/folds.scm
@@ -0,0 +1,13 @@
+[
+  (do_statement)
+  (while_statement)
+  (repeat_statement)
+  (if_statement)
+  (for_statement)
+  (function_declaration)
+  (function_definition)
+  (parameters)
+  (arguments)
+  (table_constructor)
+  (comment)+
+] @fold
