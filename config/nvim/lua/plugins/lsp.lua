c67372137f0a8b2c9639b9f24c705444afceb0c3
alexeRadu <alexeradu2007@gmail.com>
2026-03-13 21:03:04 +0200

    nvim: lazy: configure lsp

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        },
+        config = function()
+            -- We install a list of LSP servers via mason
+            -- Mason can install external packages like LSP servers, DAP servers, Linters, Formatters
+            -- For more info about what you can install execute ':Mason'
+            servers = {
+                clangd = true,
+            }
+
+            require('mason').setup()
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "clangd",           -- LSP for C, C++
+                    "lua_ls",           -- LSP for Lua
+                    "pyright",          -- LSP for Python
+                }
+            })
+
+            vim.lsp.enable('clangd')
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local map       = require('helpers').map
+                    local telescope = require('telescope.builtin')
+
+                    map('n', 'gd', vim.lsp.buf.definition)
+                    map('n', 'gr', telescope.lsp_references)
+                end,
+            })
+        end
+    }
+}
