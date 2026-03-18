13680ecea3f85973d46342f87e217ae6d3a652c5
alexeRadu <alexeradu2007@gmail.com>
2026-03-18 11:05:26 +0200

    nvim: treesitter: update config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,51 +3,66 @@ return {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
         build = ':TSUpdate',
+        branch = 'main',
         config = function()
-            require('nvim-treesitter').setup {
-                ensure_installed = {
-                    'c',
-                    'cpp',
-                    'lua',
-                    'vim',
-                    'python',
-                    'bash',
-                    'cmake',
-                    'make',
-                    'ninja',
-                    'diff',
-                    'gitattributes',
-                    'gitcommit',
-                    'json',
-                    'markdown',
-                    'java',
-                    'javascript',
-                    'typescript',
-                    'html',
-                    'css',
-                    'go',
-                },
-
-                sync_install = false,
-
-                highlight = {
-                    enable = true,
-                },
-
-                indent = {
-                    enable = true,
-                },
-
-                playground = {
-                    enable = true
-                },
-
-                query_linter = {
-                    enable = true,
-                    use_virtual_text = true,
-                    lint_events = {"BufWrite", "CursorHold"},
-                },
+            require('nvim-treesitter').install {
+                'c',
+                'cpp',
+                'lua',
+                'vim',
+                'python',
+                'bash',
+                'cmake',
+                'make',
+                'ninja',
+                'diff',
+                'gitattributes',
+                'gitcommit',
+                'json',
+                'markdown',
+                'java',
+                'javascript',
+                'typescript',
+                'html',
+                'css',
+                'go',
             }
+
+            local group = vim.api.nvim_create_augroup("custom-treesitter", {clear = true})
+            vim.api.nvim_create_autocmd("FileType", {
+                group = group,
+                pattern = '*',
+                callback = function(args)
+                    local bufnr = args.buf
+                    local ft    = vim.bo[bufnr].filetype
+                    local lang  = vim.treesitter.language.get_lang(ft)
+
+                    if lang == nil then
+                        print("Lang nil?")
+                        return
+                    end
+
+                    print("Found ft: " .. vim.inspect(ft) .. ", lang: " .. vim.inspect(lang))
+
+                    if not vim.treesitter.language.add(lang) then
+                        local available = vim.g.ts_available or require('nvim-treesitter').get_available()
+                        if not vim.g.ts_available then
+                            vim.g.ts_available = available
+                        end
+
+                        if vim.tbl_contains(available, lang) then
+                            vim.print(string.format("Installing parsers nad queries for %s", lang))
+                            require('nvim-treesitter').install(lang)
+                        end
+                    end
+
+                    if vim.treesitter.language.add(lang) then
+                        -- start treesitter highlighting
+                        print("starting ...")
+                        vim.treesitter.start(args.buf, lang)
+                    end
+                end,
+            })
         end
     }
 }
