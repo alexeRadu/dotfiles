1968b9764922aade25c74653d08a590cf63a4a75
alexeRadu <alexeradu2007@gmail.com>
2026-03-18 11:04:30 +0200

    nvim: telescope: update configuration
    - switch to main branch (default)
    - install 'ensured_languages' at start (if not)
    - lazy start/install additional packages

--- config/nvim/lua/helpers.lua
+++ config/nvim/lua/helpers.lua
@@ -4,4 +4,14 @@ M.map = function(mode, lhs, rhs, desc)
     vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc})
 end
 
+M.get_visual = function()
+    local _, ls, cs = unpack(vim.fn.getpos("v"))
+    local _, le, ce = unpack(vim.fn.getpos("."))
+
+    ls, le = math.min(ls, le), math.max(ls, le)
+    cs, ce = math.min(cs, ce), math.max(cs, ce)
+
+    return vim.api.nvim_buf_get_text(0, ls - 1, cs -1, le - 1, ce, {})[1] or ""
+end
+
 return M
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,27 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
         dependencies = {
            'nvim-lua/plenary.nvim',
-           {
-               'nvim-telescope/telescope-fzf-native.nvim',
-               run = 'make',
-               cond = vim.fn.executable('make') == 1,
-           }
+           { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
         config = function()
 
             -- Keymaps
             local map = require('helpers').map
             local telescope = require('telescope.builtin')
+            local get_visual = require('helpers').get_visual
+
+            local live_grep_selected = function()
+                local selection = get_visual()
+
+                telescope.live_grep({ default_text = string.format([[%s]], selection) })
+            end
 
             map('n', '<leader>f', telescope.find_files, "Find Files")
             map('n', '<leader>b', telescope.buffers,    "Open Buffers")
             map('n', '<leader>g', telescope.live_grep,  "Grep")
-            -- vim.keymap.set('v', '<leader>g', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', { silent = true })
-            -- vim.keymap.set("n", "<leader>y", ":Telescope file_browser<CR>", { silent = true })
+            map('v', '<leader>g', live_grep_selected,   "Live Grep")
         end
     }
 }
